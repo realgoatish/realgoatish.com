354c29210bf5486bf1df40f1191471a03474059e
Michael Lamb <mflambjr@gmail.com>
2021-11-12 02:45:29 -0500

    add type definitions

---
+++ types/constants.d.ts
@@ -0,0 +1,11 @@
+export namespace headingLevel {
+    const name: string;
+}
+export namespace headerContext {
+    const name_1: string;
+    export { name_1 as name };
+}
+export namespace counter {
+    const name_2: string;
+    export { name_2 as name };
+}

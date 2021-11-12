1d131d105583de84e0ae02c0e47474adeea49924
Michael Lamb <mflambjr@gmail.com>
2021-11-11 22:13:29 -0500

    add node build scripts to generate SVG sprite

---
+++ imagemin-svgo.js
@@ -0,0 +1,20 @@
+import imagemin from 'imagemin';
+import imageminSvgo from 'imagemin-svgo';
+
+(async () => {
+  await imagemin(['static/svg/*.svg'], {
+    destination: 'temp/svg',
+    plugins: [
+      imageminSvgo({
+        plugins: [
+          {name: 'cleanupIDs', params: {remove: false}},
+          {name: 'cleanupNumericValues', params: {floatPrecision: 2}},
+          {name: 'removeStyleElement'},
+          {name: 'removeTitle'}
+        ],
+        multipass: true
+      })
+    ]
+  })
+  console.log('SVG-Icons were successfully optimized');
+})()

939ab36b341c1843d0bfb5720afb47f8e6cf19ee
Michael Lamb <mflambjr@gmail.com>
2022-04-03 23:25:08 -0400

    type changes

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -4,7 +4,26 @@
  * @param {string} value
  * @returns {boolean}
  */
-export function isDeclarationSupported(
-	property: string,
-	value: string
-): boolean;
+export function isDeclarationSupported(property: string, value: string): boolean;
+export function linkResolver(doc: any): string;
+export function initApi(req: any, endpoint: any): Promise<import("@prismicio/client/types/ResolvedApi").default>;
+export function getGlobalLayoutData(api: any): any;
+export function getHomePageData(api: any): any;
+export function getAllPostsPageData(api: any): any;
+export function getAllPosts(api: any): any;
+export function getPost(api: any, slug: any): any;
+export function getAllRoutes(api: any): any;
+export const monthMap: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+    5: string;
+    6: string;
+    7: string;
+    8: string;
+    9: string;
+    10: string;
+    11: string;
+};

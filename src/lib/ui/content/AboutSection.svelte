3da06e2f2e774fa9def4bb4a1f0588ba2d99022b
Michael Lamb <mflambjr@gmail.com>
2022-04-04 03:14:21 -0400

    add fade animations

--- src/lib/ui/content/AboutSection.svelte
+++ src/lib/ui/content/AboutSection.svelte
@@ -4,11 +4,10 @@
   import Stack from '$lib/toolbox/layout/Stack.svelte'
   import Icon from '$lib/toolbox/layout/Icon.svelte'
   import Center from '$lib/toolbox/layout/Center.svelte'
-
-
   import Section from '$lib/toolbox/section/Section.svelte'
   import Article from '$lib/toolbox/article/Article.svelte'
   import HeadingTag from '$lib/toolbox/heading-tag/HeadingTag.svelte'
+  import Render from '$lib/toolbox/render-on-scroll/RenderOnScroll.svelte'
 
   export let title
 
@@ -22,27 +21,31 @@
   <Section>
     <Cover>
       <Section slot="header">
-        <Stack space="var(--s0)">
-          <HeadingTag message={title} />
-          {@html summary}
-        </Stack>
+        <Render>
+          <Stack space="var(--s0)" >
+            <HeadingTag message={title} />
+            {@html summary}
+          </Stack>
+        </Render>
       </Section>
       <Switcher slot="featured" space="var(--s1)" threshold={"25rem"} wrapperElement="div">
         {#each sections as section, i}
-          <Article>
-            <Center andText={true} intrinsic={true}>
-              <Stack>
-                {#if i === 1}
-                  <HeadingTag message={section.header} />
-                    <Icon ariaHidden={true} space={null} iconId="#trending-up" />
-                {:else}
-                  <HeadingTag message={section.header} />
-                    <Icon ariaHidden={true} space={null} iconId="#light-bulb" />
-                {/if}
-                {@html section.description}
-              </Stack>
-            </Center>
-          </Article>
+          <Render>
+            <Article>
+              <Center andText={true} intrinsic={true}>
+                <Stack>
+                  {#if i === 1}
+                    <HeadingTag message={section.header} />
+                      <Icon ariaHidden={true} space={null} iconId="#trending-up" />
+                  {:else}
+                    <HeadingTag message={section.header} />
+                      <Icon ariaHidden={true} space={null} iconId="#light-bulb" />
+                  {/if}
+                  {@html section.description}
+                </Stack>
+              </Center>
+            </Article>
+          </Render>
         {/each}
       </Switcher>
       <!-- <button slot="footer">PRESS ME</button> -->
@@ -56,9 +59,25 @@
     margin-block: var(--s5);
   }
 
-  div :global(.switcher > article) {
+  div :global(.switcher > div > article) {
     margin-block-start: var(--s5);
   }
+
+  div :global(.switcher > div > article),
+  div :global(section > div > .stack) {
+	  animation: fade-in-bottom 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+  }
+
+  @keyframes -global-fade-in-bottom {
+  0% {
+    transform: translateY(50px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
 
   div :global(.icon) {
     font-size: var(--s4);

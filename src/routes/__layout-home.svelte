e7a60d7279eb323f35aadcc7db594cb429d8f47f
Michael Lamb <mflambjr@gmail.com>
2022-07-08 22:45:10 -0400

    switch to using 'stuff' for SEO & leverage named layouts better

--- src/routes/__layout-home.svelte
+++ src/routes/__layout-home.svelte
@@ -1,7 +1,7 @@
 <script context="module">
 
 
-	export async function load({ fetch, url, stuff }) {
+	export async function load({ fetch, url }) {
 		let res = await fetch('/getLayoutData/', {
 			method: 'GET'
 		}).then((data) => data.json());
@@ -13,8 +13,6 @@
     globalSEO.canonical = `https://${host}${path}`
     globalSEO.siteUrl = `https://${host}/`
 
-    stuff = globalSEO
-
 		return {
 			props: {
         headerData
@@ -24,23 +22,22 @@
       }
 		};
 	}
+
 </script>
 
 <script>
-
   import { navData } from '$lib/js/constants'
+
   import { setContext } from 'svelte'
   import { page } from '$app/stores'
   import '@fontsource/playfair-display/800.css';
-  import '../app.css';
+	import '../app.css';
   import Sprite from '$lib/toolbox/sprite/Sprite.svelte'
-  import Nav from '$lib/ui/nav/Nav.svelte'
-  import Footer from '$lib/ui/footer/Footer.svelte'
   export let headerData
 
   setContext(navData, headerData)
 
-  console.log(`$page.stuff in __layout.svelte: ${JSON.stringify($page.stuff, null, 2)}`)
+  console.log(`$page.stuff in __layout-home.svelte: ${JSON.stringify($page.stuff, null, 2)}`)
 
 </script>
 
@@ -51,30 +48,15 @@
 </svelte:head>
 
 <div id="body-wrapper">
-  <header>
-    <Nav />
-  </header>
   <slot />
-  <Footer />
 </div>
 
 <style>
-
-  div :global(.heading-tag) {
-    font-family: 'Playfair Display';
+	div :global(.heading-tag),
+  div :global(h1),
+  div :global(h2) {
+		font-family: 'Playfair Display';
     color: var(--color-lightish);
-  }
+	}
 
-  /* BEGIN CODE BLOCK FOR PINNING FOOTER TO BOTTOM OF PAGE */
-
-  div :global(main) {
-    display: block;
-  }
-
-  div {
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-  }
-
-  /* END CODE BLOCK FOR PINNING FOOTER TO BOTTOM OF PAGE */
 </style>

2be4dc45e0f71eaa5c943f56c03f9bd0d878e07e
Michael Lamb <mflambjr@gmail.com>
2022-07-08 22:41:16 -0400

    changes to SK config setup w/ upgrade

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,15 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
-import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: vercel(),
-    trailingSlash: 'always',
-		vite: {
-			resolve: {
-				alias: {
-					$static: path.resolve('static')
-				}
-			}
-		}
+    trailingSlash: 'always'
 	}
 };
 

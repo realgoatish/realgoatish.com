05427cd27c8ae9b38b84f873b78409662c70dbca
Michael Lamb <mflambjr@gmail.com>
2022-07-08 22:55:36 -0400

    reorg/rename files for shadow endpoints & named layouts

--- src/routes/blog/[slug]/index.svelte
+++ src/routes/blog/[slug]/index.svelte
@@ -1,46 +1,33 @@
 <script context="module">
-  import Center from '$lib/toolbox/layout/Center.svelte'
-  import Stack from '$lib/toolbox/layout/Stack.svelte'
-  import Frame from '$lib/toolbox/layout/Frame.svelte'
+  import { Center, Stack, Frame, Cover } from '@realgoatish/svelte-every-layout'
   import Image from '$lib/toolbox/layout/Image.svelte'
-  import Cover from '$lib/toolbox/layout/Cover.svelte'
   import Section from '$lib/toolbox/section/Section.svelte'
   import Article from '$lib/toolbox/article/Article.svelte'
   import HeadingTag from '$lib/toolbox/heading-tag/HeadingTag.svelte'
   import BaseSEO from '$lib/toolbox/seo/BaseSEO.svelte'
 
-	export async function load({ fetch, params }) {
+  export const load = async ({ props }) => {
 
-    const { slug } = params
+    const { pageSEO } = props
 
-		let res = await fetch(`/api/blog/${slug}/`, {
-			method: 'GET'
-		}).then((data) => data.json());
-
-    console.log(`res before processing: ${JSON.stringify(res, null, 2)}`)
-
-    const { post, pageSEO } = res
-
-
-		return {
-			props: {
-				post,
+    return {
+      props,
+      stuff: {
         pageSEO
-			}
-		};
-	}
+      }
+    }
+  }
+
+
 </script>
 
 <script>
-  import { globalSeoData } from '$lib/js/constants'
-  import { getContext, onMount } from 'svelte'
+  import { onMount } from 'svelte'
+  import { page } from '$app/stores'
   import 'prismjs/themes/prism.min.css'
   import Prism from 'prismjs'
   import 'prism-svelte'
   export let post
-  export let pageSEO
-
-  const globalSEO = getContext(globalSeoData)
 
   onMount(() => {
 
@@ -56,17 +43,19 @@
     })
   })
 
+  $: console.log(`$page.stuff on /blog/[slug] page: ${JSON.stringify($page.stuff, null, 2)}`)
+
 </script>
 
 <BaseSEO data={{
-  currentPage: pageSEO,
-  global: globalSEO
+  currentPage: $page.stuff.pageSEO,
+  global: $page.stuff.globalSEO
 }}/>
 
 <main>
-  <Center intrinsic={true}>
+  <Center intrinsic={true} gutters={"var(--s-3)"} max={"70ch"}>
     <Article>
-      <Cover>
+      <Cover noPad={true} >
         <div slot="header">
           <Stack>
             <HeadingTag message={post.title} />
@@ -74,7 +63,7 @@
           </Stack>
         </div>
         <Section slot="featured">
-          <Stack>
+          <Stack space={"var(--s1)"}>
             {#each post.postSections as section}
               {#if section.postBlockLayout === 'Text'}
                 <div class="blog-post-text">{@html section.text}</div>
@@ -83,7 +72,7 @@
               {:else if section.postBlockLayout === 'Image'}
                 <div class="blog-post-image">
                   <Frame>
-                    <Image images={section.image} altText={section.altText} />
+                    <Image lazy={true} images={section.image} altText={section.altText} />
                   </Frame>
                 </div>
               {:else if section.postBlockLayout === 'Title Image'}
@@ -92,7 +81,7 @@
                     <HeadingTag message={section.title} />
                     <div class="blog-post-image">
                       <Frame>
-                        <Image images={section.image} altText={section.altText} />
+                        <Image lazy={true} images={section.image} altText={section.altText} />
                       </Frame>
                     </div>
                   </Stack>
@@ -103,7 +92,7 @@
                     <HeadingTag message={section.title} />
                     <div class="blog-post-image">
                       <Frame>
-                        <Image images={section.image} altText={section.altText} />
+                        <Image lazy={true} images={section.image} altText={section.altText} />
                       </Frame>
                     </div>
                     <div class="blog-post-text">{@html section.text}</div>
@@ -123,7 +112,7 @@
                     <div class="blog-post-text">{@html section.text}</div>
                     <div class="blog-post-image">
                       <Frame>
-                        <Image images={section.image} altText={section.altText} />
+                        <Image lazy={true} images={section.image} altText={section.altText} />
                       </Frame>
                     </div>
                   </Stack>
@@ -147,14 +136,15 @@
 
 <style>
 
-  main :global(.center) {
+  /* main :global(.center) {
     --gutters: var(--s-3);
     --measure: 70ch;
-  }
+  } */
 
   main :global(.cover) {
-    padding-top: var(--s1);
-    padding-bottom: var(--s1);
+    /* padding-top: var(--s1);
+    padding-bottom: var(--s1); */
+    padding-block: var(--s1);
   }
 
   main :global(pre.language-) {
@@ -170,9 +160,9 @@
     padding-left: 45%;
   }
 
-  main :global(article > section > .stack) {
+  /* main :global(article > section > .stack) {
     --space: var(--s1);
-  }
+  } */
 
   main .blog-post-text > :global(* + *) {
     margin-top: var(--s0);
---
+++ src/routes/index@home.svelte
@@ -0,0 +1,46 @@
+<script context="module">
+
+  import Header from '$lib/ui/header/Header.svelte'
+  import { Center } from '@realgoatish/svelte-every-layout'
+  import AboutSection from '$lib/ui/content/AboutSection.svelte'
+  import BaseSEO from '$lib/toolbox/seo/BaseSEO.svelte'
+  import Footer from '$lib/ui/footer/Footer.svelte'
+
+  export const load = async ({ props }) => {
+
+    const { pageSEO } = props
+
+    return {
+      props,
+      stuff: {
+        pageSEO
+      }
+    }
+  }
+
+</script>
+
+<script>
+
+  import { page } from '$app/stores'
+
+  export let hero
+
+  export let about
+
+  $: console.log(`$page.stuff on home page: ${JSON.stringify($page.stuff, null, 2)}`)
+
+</script>
+
+<BaseSEO data={{
+  currentPage: $page.stuff.pageSEO,
+  global: $page.stuff.globalSEO
+}}/>
+
+<Header {...hero} />
+<main>
+  <Center max={"var(--measure)"}>
+    <AboutSection {...about} />
+  </Center>
+</main>
+<Footer />
